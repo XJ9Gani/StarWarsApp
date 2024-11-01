bb29e955369927452d35d3d24389fdc8d071190a
Ваше_имя <_email>
2024-11-01 20:22:52 +0500

    all done

--- src/pages/CharacterPage.tsx
+++ src/pages/CharacterPage.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Loader from "../components/Loader";
 import "../style/CharInfo.scss";
